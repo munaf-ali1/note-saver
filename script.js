a9db755755b63279fe183d7aaab061635b0ec197
munaf ali <munaf8969924620@gmail.com>
2025-05-06 00:33:20 +0530

    some changes in files

---
+++ script.js
@@ -0,0 +1,39 @@
+ //initialize a project with npm
+ // express install
+ const express = require('express');
+ const app  = express();
+ const path = require('path');
+ const fs = require('fs');
+
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true}));
+ 
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.set('view engine','ejs');
+
+ app.get("/",function(req,res){
+   fs.readdir(`./files`,function(err, files){
+      res.render("index", {files: files});
+      
+
+   })
+    
+ });
+
+ app.post('/create',function(req, res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
+      res.redirect("/")
+
+   });
+
+
+ })
+ 
+ // dynamic routing ->
+
+ //app.get("/profile/:username",function(req,res){
+  //  res.send(`welcome, ${req.params.username}`);
+ //});
+
+ app.listen(3000)
+  

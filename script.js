99658c311d5fd14a2ff384c60da962b048c84848
munaf ali <munaf8969924620@gmail.com>
2025-05-08 00:41:50 +0530

    .

--- script.js
+++ script.js
@@ -19,6 +19,27 @@
    })
     
  });
+ 
+ app.get('/edit/:filename',function(req,res){
+     res.render("edit",{filename: req.params.filename})
+ })
+  
+ app.get('/edit',function(req,res){
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+      res.redirect("/");
+    })
+
+ 
+  
+
+ })
+
+
+ app.get("/file/:filename",function(req,res){
+  fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err,filedata){
+    res.render("show", {filename: req.params.filename, filedata: filedata})
+  })
+ })
 
  app.post('/create',function(req, res){
    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
